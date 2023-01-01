9237dd228f4c5f92d5f0a3528e5a37b61436dceb
moipo <moipochtoviyyashik@inbox.ru>
2023-01-01 14:55:05 +0300

    add ability to fold and unfold menus independently

--- project/app/views.py
+++ project/app/views.py
@@ -8,15 +8,17 @@ class Main:
         return render(request,'main.html',ctx)
 
     def page(request, page_num=1):
-        new_active_item = request.GET.get("new_active_item")
-        print(request.GET)
-        previous_unfolded_items = list(map(int,request.GET.get("unfolded_items").split()))
-        if new_active_item is not None:
-            active_item = Level.objects.filter(title = new_active_item)[0]
+        new_active_item_id = request.GET.get("new_active_item_id")
+        new_disabled_item_id = request.GET.get("new_disabled_item_id")
 
+        previous_unfolded_items_id = []
+        unfolded_items_id = request.GET.get("unfolded_items_id")
+        if unfolded_items_id is not None:
+            previous_unfolded_items_id = list(map(int,unfolded_items_id.split()))
 
+        if new_active_item_id is not None:
 
-
+            active_item = Level.objects.filter(id = new_active_item_id)[0]
             unfolded_items = [active_item]
 
             while not active_item.is_menu:
@@ -24,12 +26,23 @@ class Main:
                 unfolded_items.append(active_item)
 
             pk_list = [i.pk for i in unfolded_items]
-            unfolded_items = Level.objects.filter(pk__in=pk_list) | Level.objects.filter(id__in=previous_unfolded_items)
+            unfolded_items = Level.objects.filter(pk__in=pk_list) | Level.objects.filter(id__in=previous_unfolded_items_id)
             unfolded_items_id = " ".join([str(i.id) for i in unfolded_items])
             ctx = {
             "unfolded_items":unfolded_items,
             "unfolded_items_id":unfolded_items_id,
             }
             return render(request,'main.html',ctx)
-        ctx = {"unfolded_items":None,}
-        return render(request,'main.html',ctx)
+        elif new_disabled_item_id is not None:
+            new_disabled_item_id = int(new_disabled_item_id)
+            if new_disabled_item_id in previous_unfolded_items_id:
+                previous_unfolded_items_id.remove(new_disabled_item_id)
+            unfolded_items_id = " ".join([str(i) for i in previous_unfolded_items_id]) if previous_unfolded_items_id is not None else ""
+            unfolded_items = Level.objects.filter(pk__in=previous_unfolded_items_id)
+            ctx = {
+            "unfolded_items":unfolded_items,
+            "unfolded_items_id":unfolded_items_id,
+            }
+            return render(request,'main.html',ctx)
+        else:
+            return render(request,'main.html',{})
