f13e67fba10322b53b6b25d988766c9ba4013000
moipo <moipochtoviyyashik@inbox.ru>
2023-01-01 15:05:55 +0300

    optimized db queries

--- project/app/views.py
+++ project/app/views.py
@@ -17,17 +17,13 @@ class Main:
             previous_unfolded_items_id = list(map(int,unfolded_items_id.split()))
 
         if new_active_item_id is not None:
+            new_active_item_id = int(new_active_item_id)
+            if new_active_item_id not in previous_unfolded_items_id:
+                previous_unfolded_items_id.append(new_active_item_id)
 
-            active_item = Level.objects.filter(id = new_active_item_id)[0]
-            unfolded_items = [active_item]
+            unfolded_items_id = " ".join([str(i) for i in previous_unfolded_items_id]) if previous_unfolded_items_id is not None else ""
+            unfolded_items = Level.objects.filter(pk__in=previous_unfolded_items_id)
 
-            while not active_item.is_menu:
-                active_item = active_item.parent_level
-                unfolded_items.append(active_item)
-
-            pk_list = [i.pk for i in unfolded_items]
-            unfolded_items = Level.objects.filter(pk__in=pk_list) | Level.objects.filter(id__in=previous_unfolded_items_id)
-            unfolded_items_id = " ".join([str(i.id) for i in unfolded_items])
             ctx = {
             "unfolded_items":unfolded_items,
             "unfolded_items_id":unfolded_items_id,
