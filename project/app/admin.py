6318c03c0e053c14f4e8d4f70f804ed4acc849a6
moipo <moipochtoviyyashik@inbox.ru>
2023-01-01 17:01:51 +0300

    add frontend

--- project/app/admin.py
+++ project/app/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class PageAdmin(admin.ModelAdmin):
-    exclude = tuple()
-    class Meta:
-        model = Page
-admin.site.register(Page, PageAdmin)
 
 class LevelAdmin(admin.ModelAdmin):
     exclude = tuple()
--- project/app/models.py
+++ project/app/models.py
@@ -2,20 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class Page(models.Model):
-    number = models.IntegerField()
-
-    def __str__(self):
-        return str(self.number)
-
 
 class Level(models.Model):
     title = models.CharField(max_length = 3000 , blank = True, default = '')
-    link = models.CharField(max_length = 3000 , blank = True, default = '')
-    # has_nested_data = models.BooleanField(default = False, blank = True, null = True)
-    is_active = models.BooleanField(default = False, blank = True, null = True)
     is_menu = models.BooleanField(default=False, blank = True, null = True)
-    page = models.ForeignKey("Page", blank = True, null = True, on_delete = models.CASCADE )
     parent_level = models.ForeignKey("Level", blank = True, null = True, on_delete = models.CASCADE )
 
     def __str__(self):
