c26c737618d6af9fb28cff0345f110a1e0b91f68
moipo <moipochtoviyyashik@inbox.ru>
2023-01-01 15:13:04 +0300

    fixed zero arguments get request problem

--- project/app/views.py
+++ project/app/views.py
@@ -8,6 +8,7 @@ class Main:
         return render(request,'main.html',ctx)
 
     def page(request, page_num=1):
+
         new_active_item_id = request.GET.get("new_active_item_id")
         new_disabled_item_id = request.GET.get("new_disabled_item_id")
 
@@ -16,6 +17,7 @@ class Main:
         if unfolded_items_id is not None:
             previous_unfolded_items_id = list(map(int,unfolded_items_id.split()))
 
+        ctx = {}
         if new_active_item_id is not None:
             new_active_item_id = int(new_active_item_id)
             if new_active_item_id not in previous_unfolded_items_id:
@@ -23,22 +25,19 @@ class Main:
 
             unfolded_items_id = " ".join([str(i) for i in previous_unfolded_items_id]) if previous_unfolded_items_id is not None else ""
             unfolded_items = Level.objects.filter(pk__in=previous_unfolded_items_id)
-
             ctx = {
             "unfolded_items":unfolded_items,
             "unfolded_items_id":unfolded_items_id,
             }
-            return render(request,'main.html',ctx)
         elif new_disabled_item_id is not None:
             new_disabled_item_id = int(new_disabled_item_id)
             if new_disabled_item_id in previous_unfolded_items_id:
                 previous_unfolded_items_id.remove(new_disabled_item_id)
             unfolded_items_id = " ".join([str(i) for i in previous_unfolded_items_id]) if previous_unfolded_items_id is not None else ""
             unfolded_items = Level.objects.filter(pk__in=previous_unfolded_items_id)
+
             ctx = {
             "unfolded_items":unfolded_items,
             "unfolded_items_id":unfolded_items_id,
             }
-            return render(request,'main.html',ctx)
-        else:
-            return render(request,'main.html',{})
+        return render(request,'main.html',ctx)
